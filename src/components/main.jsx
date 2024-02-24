2f93dc2a795333b4930faeef9239be2eff77fd45
lokessh kumar <lokeshnallamolu43@gmail.com>
2024-02-24 01:50:28 -0800

    Starwar

---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Main = () => {
+  return (
+    <div className='main' style={{background:"black",height:"1000px"}}>
+      Main
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from "../assets/logo.png"
+import "../components/nav.css"
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-light bg-light sticky-top">
+      <div className="container-fluid">
+          <a className="navbar-brand"  href="#">
+            <img src={Logo} alt="" width="35" height="30" className="d-inline-block align-text-top"/>
+            <span>  <b> PLANETS</b></span>
+          </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
